d5de0bbe52dcab2ceb20a73a4786395b35168b6a
sama26 <58335184+sama26@users.noreply.github.com>
2021-02-24 12:18:29 +0000

    Tables created and tested

--- sql_queries.py
+++ sql_queries.py
@@ -1,27 +1,68 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP table IF EXISTS songplays"
+user_table_drop = "DROP table IF EXISTS users"
+song_table_drop = "DROP table IF EXISTS songs"
+artist_table_drop = "DROP table IF EXISTS artists"
+time_table_drop = "DROP table IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
-""")
+songplay_table_create = ("CREATE TABLE IF NOT EXISTS songplays"
+                         "("
+                         "songplay_id int, "
+                         "start_time time, "
+                         "user_id int, "
+                         "level varchar, "
+                         "song_id varchar, "
+                         "artist_id varchar, "
+                         "session_id int, "
+                         "location varchar, "
+                         "user_agent varchar"
+                         ");"
+                         )
 
-user_table_create = ("""
-""")
+user_table_create = ("CREATE TABLE IF NOT EXISTS users"
+                     "("
+                     "user_id int, "
+                     "first_name varchar, "
+                     "last_name varchar, "
+                     "gender varchar, "
+                     "level varchar"
+                     ");"
+                     )
 
-song_table_create = ("""
-""")
+song_table_create = ("CREATE TABLE IF NOT EXISTS songs"
+                     "("
+                     "song_id varchar, "
+                     "title varchar, "
+                     "artist_id varchar, "
+                     "year int, "
+                     "duration float"
+                     ");"
+                     )
 
-artist_table_create = ("""
-""")
+artist_table_create = ("CREATE TABLE IF NOT EXISTS artists"
+                       "("
+                       "artist_id varchar, "
+                       "name varchar, "
+                       "location varchar, "
+                       "latitude float, "
+                       "longitude float"
+                       ");"
+                       )
 
-time_table_create = ("""
-""")
+time_table_create = ("CREATE TABLE IF NOT EXISTS time"
+                     "("
+                     "start_time time, "
+                     "hour int, "
+                     "day int, "
+                     "week int, "
+                     "month int, "
+                     "year int, "
+                     "weekday int"
+                     ");"
+                     )
 
 # INSERT RECORDS
 
