f12d3c835f76bee5d5a1fd501a66ce1328c0d55d
sama26 <58335184+sama26@users.noreply.github.com>
2021-03-03 11:52:11 +0000

    Updated sql_queries.py to include appropriate data types and NOT NULLS

--- sql_queries.py
+++ sql_queries.py
@@ -10,9 +10,9 @@ time_table_drop = "DROP table IF EXISTS time"
 
 songplay_table_create = ("CREATE TABLE IF NOT EXISTS songplays"
                          "("
-                         "songplay_id int PRIMARY KEY, "
-                         "start_time time, "
-                         "user_id int, "
+                         "songplay_id serial PRIMARY KEY, "
+                         "start_time time NOT NULL, "
+                         "user_id int NOT NULL, "
                          "level varchar, "
                          "song_id varchar, "
                          "artist_id varchar, "
@@ -67,23 +67,30 @@ time_table_create = ("CREATE TABLE IF NOT EXISTS time"
 # INSERT RECORDS
 
 songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id,
-                         location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s); 
+                         location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                         ON CONFLICT DO NOTHING; 
                          """)
 
 user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) 
-                     VALUES (%s, %s, %s, %s, %s); 
+                     VALUES (%s, %s, %s, %s, %s)
+                     ON CONFLICT (user_id)
+                     DO UPDATE
+                        SET level = EXCLUDED.level; 
                      """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) 
-                     VALUES (%s, %s, %s, %s, %s); 
+                     VALUES (%s, %s, %s, %s, %s)
+                     ON CONFLICT DO NOTHING; 
                      """)
 
 artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) 
-                       VALUES (%s, %s, %s, %s, %s); 
+                       VALUES (%s, %s, %s, %s, %s)
+                       ON CONFLICT DO NOTHING; 
                        """)
 
 time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) 
-                     VALUES (%s, %s, %s, %s, %s, %s, %s); 
+                     VALUES (%s, %s, %s, %s, %s, %s, %s)
+                     ON CONFLICT DO NOTHING; 
                      """)
 
 # FIND SONGS
